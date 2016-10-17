fedf6038476cae3cd64b1fce310d7dfa37f44ec2
Abhi <abhishekam.swamy@gmail.com>
2016-10-17 23:48:11 +0530

    Adds basic javascript configurations

--- init.el
+++ init.el
@@ -12,3 +12,17 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/configurations.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smartparens js2-mode monokai-theme exec-path-from-shell emacs-path-from-shell elpy yasnippet which-key use-package try pyvenv org-bullets evil-workman-mode counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
